630188ba3dc4a3d6b8dbea949f1d79cd3cba59b8
Thomas Barkshire <thomasbarkshire@gmail.com>
2026-05-05 12:38:15 +0100

    Created simple API port for localhost

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () =>{
+    console.log(`Simple api listening on port ${port}`)
+})
