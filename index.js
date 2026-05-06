4cce1cb73a2bede7265e601e0149c731b9be0eaa
Thomas Barkshire <thomasbarkshire@gmail.com>
2026-05-06 09:14:39 +0100

    Rebuilt index.js to function with stack programs instead of monolithic

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const app = express()
+const app = require('./app')
+
+// const express = require("express")
+// const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//     res.send({"Welcome":"Hello World"})
+// })
+
+// app.get('/query',(req, res) => {
+//     res.send(req.query)
+// })
+
+// app.get('/param/:id', (req, res) => {
+//     res.send(req.params)
+// })
 
 app.listen(port, () =>{
     console.log(`Simple api listening on port ${port}`)
